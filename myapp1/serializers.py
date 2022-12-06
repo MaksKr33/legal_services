3ae24fef67903797c2be83310ed0552343ec9de1
MaksKr33 <makskrypich@gmail.com>
2022-12-06 17:53:57 +0200

    writing docstring

--- myapp1/serializers.py
+++ myapp1/serializers.py
@@ -8,7 +8,6 @@ class OrderSeriaLizer(ModelSerializer):
     status_case = serializers.SlugRelatedField( many=False,queryset=StatusCase.objects.all(), slug_field='NameStatusCase')
     class Meta:
         model = Baza_client
-        
         fields = ["id",'name_client', 'contract_number', 'date_contract', 'type_case', 'date', 'status_case', 'documents_case']
 
 class StatusCaseSeriaLizer(ModelSerializer):
